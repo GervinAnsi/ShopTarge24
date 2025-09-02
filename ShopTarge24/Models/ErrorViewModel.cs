8769774c368e4a836e5716e86e0afc0645e6984f
GervinAnsi <158846288+GervinAnsi@users.noreply.github.com>
2025-09-02 09:37:00 +0300

    init

---
+++ ShopTarge24/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ShopTarge24.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
