d32cf130af78ef43c7e91593b7d7b2999c20c5b3
GervinAnsi <158846288+GervinAnsi@users.noreply.github.com>
2025-09-16 11:48:20 +0300

    KindergartenDomainObject

--- ShopTarge24.Core/Domain/Kindergarten.cs
+++ ShopTarge24.Core/Domain/Kindergarten.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ShopTarge24.Core.Domain
+﻿namespace ShopTarge24.Core.Domain
 {
-    internal class Kindergarten
+    public class Kindergarten
     {
+        public Guid Id { get; set; }
+        public string GroupName { get; set; }
+        public int ChildrenCount { get; set; }
+        public string KindergartenName { get; set; }
+        public string TeacherName {  get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
     }
 }
