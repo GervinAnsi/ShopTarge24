e5a08e3f9ac3296466d4f783da4d548dfc3561ec
GervinAnsi <158846288+GervinAnsi@users.noreply.github.com>
2025-09-30 13:26:11 +0300

    UpdateDBRealEstate

--- ShopTarge24.Data/ShopTarge24Context.cs
+++ ShopTarge24.Data/ShopTarge24Context.cs
@@ -14,5 +14,6 @@ namespace ShopTarge24.Data
         public DbSet<Spaceships> Spaceships { get; set; }
 
         public DbSet<FileToApi> FileToApis { get; set; }
+        public DbSet<RealEstate> RealEstates { get; set; }
     }
 }
