18264b3a34ce561ba6a8c425183d5b7a9460f98c
GervinAnsi <158846288+GervinAnsi@users.noreply.github.com>
2026-01-05 11:28:54 +0200

    ResetPasswordWorking

--- ShopTarge24/Models/Accounts/ResetPasswordViewModel.cs
+++ ShopTarge24/Models/Accounts/ResetPasswordViewModel.cs
@@ -1,6 +1,20 @@
-﻿namespace ShopTarge24.Models.Accounts
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShopTarge24.Models.Accounts
 {
     public class ResetPasswordViewModel
     {
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+
+        public string Token { get; set; }
     }
 }
